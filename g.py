0785eeab97d0384ffb9efaee55442e2433d6c514
XxULTRADARKWIZARDxX <rchen2009@duck.com>
2025-01-27 14:50:17 -0800

    added a lisst
    I need money

--- g.py
+++ g.py
@@ -1,12 +1,15 @@
 import datetime
 # adding date
 
+storage = []
+
 def add_expense(cat, date, money)
     try:
         date.datetime.striptime(date, '%Y-%m-%d' )
     except ValueError:
         print("Please try again with the format of YYYY-MM-DD")
         return
+
 
 
 

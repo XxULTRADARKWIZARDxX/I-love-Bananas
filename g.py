c7c8f2a8ffe5d7e2c926059fba2010993d2bf00e
XxULTRADARKWIZARDxX <rchen2009@duck.com>
2025-01-27 14:45:41 -0800

    my secnd  revision code
    added a way to track time

--- g.py
+++ g.py
@@ -1,1 +1,13 @@
-print ("hello worll")
+import datetime
+# adding date
+
+def add_expense(cat, date, money)
+    try:
+        date.datetime.striptime(date, '%Y-%m-%d' )
+    except ValueError:
+        print("Please try again with the format of YYYY-MM-DD")
+        return
+
+
+
+    
